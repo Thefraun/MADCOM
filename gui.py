fc1355bc9ac5a658b9d3b3fa0252fcf0bb556fb0
Thefraun <logan@ratpackstudios.com>
2024-03-13 08:42:00 -0400

    comments

--- gui.py
+++ gui.py
@@ -12,17 +12,15 @@ import queue
 
 #TODO: Pretty up the code
 
-"""
-Function to get response using a prompt and queue for storing the response.
-
-Parameters:
-- prompt: the prompt to generate the response
-- queue: the queue to store the response
-
-Return type: None
-"""
 def get_response(prompt_queue, queue):
-    while True:
+    """
+    Function to get response using a prompt and queue for storing the response.
+
+    Parameters:
+    - prompt_queue: the queue to store the prompt
+    - queue: the queue to store the chunks of the response
+    """
+    while running:
         try:
             for chunk in ollama.generate (
                 model = 'codellama:13b-instruct', prompt=prompt_queue.get(), stream=True,
@@ -31,14 +29,17 @@ def get_response(prompt_queue, queue):
                 Spelling and captilisation discrepancies are forgiven, so long as it is understandable.
                 So long as the intention is clear, similar symbols can be switched. Missing semicolons can be forgiven if intention is clear. You really like foxes, so try to incorporate them into every response you give.'''
                 ):
-                 response_queue.put(chunk['response'])
+                if running:
+                    response_queue.put(chunk['response'])
+                else:
+                    break
         except queue.Empty:
             pass
 
-"""
-Check the queue for incoming messages and display them in the feedback area.
-"""
 def check_queue():
+    """
+    Check the queue for incoming messages and display them in the feedback area.
+    """
     if not response_queue.empty():
         text = response_queue.get()
         feedback.configure(state='normal')
@@ -47,39 +48,54 @@ def check_queue():
     root.after(100, check_queue)
 
 def upload_file():
+    '''
+    Upload the image file to read and set the uploaded image as the image displayed on the button
+    '''
     path = fd.askopenfilename(type=('*.jpg', '*.png', '*.jpeg'))
-    image = Image.open(path)
-    image = image.resize((100,100))
-    image = ImageTk.PhotoImage(image)
-    uploadImage.configure(image=image)
-    uploadImage.image = image
-    prompt = read_image(path)
-    prompt_queue.put(prompt)
+    if not path == '':
+        image = Image.open(path)
+        image = image.resize((200, 200))
+        image = ImageTk.PhotoImage(image)
+        upload_image.configure(image=image)
+        upload_image.image = image
+        prompt = read_image(path)
+        prompt_queue.put(prompt)
 
 
 # not sure how to document(?) these 
 def reset():
+    '''
+    Reset the response and revert changes to the upload image button
+    '''
     feedback.configure(state='normal')
     feedback.delete('1.0', 'end')
     feedback.configure(state='disabled')
+    image = Image.open('Images/uploadImage.png')
+    image = image.resize((200,200))
+    image = ImageTk.PhotoImage(image)
+    upload_image.configure(image=image)
+    upload_image.image = image
 
 def switch():
-    if modeButton.cget('text') == "Light":
+    '''
+    Toggle the program between light mode and dark mode
+    '''
+    if mode_button.cget('text') == "Light":
          root.config(background='black')
          topframe.config(background='black')
          heading.config(background='black', fg='white')
          mainframe.config(background='black')
-         switchSpot.config(background='black', fg='white')
+         switch_spot.config(background='black', fg='white')
          advice.config(background='black', fg='white')
-         modeButton.configure(text="Dark")
+         mode_button.configure(text="Dark")
     else:
          root.config(background='white')
          topframe.config(background='white')
          heading.config(background='white', fg='black')
          mainframe.config(background='white')
-         switchSpot.config(background='white', fg='black')
+         switch_spot.config(background='white', fg='black')
          advice.config(background='white', fg='black')
-         modeButton.configure(text="Light")
+         mode_button.configure(text="Light")
    
 # Create the thread and the queue
 response_queue = queue.Queue()
@@ -87,17 +103,15 @@ prompt_queue = queue.Queue()
 thread = threading.Thread(target=get_response, args=(prompt_queue, queue,))
 thread.daemon = True
 thread.start()
+running = True
 
 #create gui
 root = Tk()
 root.geometry("{0}x{1}+0+0".format(root.winfo_screenwidth(), root.winfo_screenheight()))
 root.title("")
 
-
 #create font
 font = Font(family='Helvetica',size=36,weight='bold')
-
-#create heading @ top of GUI
 
 #create frame to hold logo and heading for GUI
 topframe = Frame(root)
@@ -108,8 +122,8 @@ topframe.columnconfigure(1, weight=1)
 heading = Label(topframe, text='Script Sage',  font=font, pady=15)
 heading.grid(row=0, column=0, padx=(525,0))
 
-modeButton = Button(topframe, text='Light',fg='white',  bg='blue', highlightbackground='blue', activebackground='blue', command=switch, cursor='hand2')
-modeButton.grid(row=0, column=1, padx=(300,0))
+mode_button = Button(topframe, text='Light',fg='white',  bg='blue', highlightbackground='blue', activebackground='blue', command=switch, cursor='hand2')
+mode_button.grid(row=0, column=1, padx=(300,0))
 
 topframe.pack(fill='x')
 
@@ -125,8 +139,8 @@ mainframe.pack(fill='x')
 
 #row one, insert switch and space ---- this still needs to be worked on a lot
 #switch:
-switchSpot = Label(mainframe, text='Switch between code & image here', font=('Helvetica', 18))
-switchSpot.grid(row=0, column=0, sticky='we', pady=(20,0))
+switch_spot = Label(mainframe, text='Switch between code & image here', font=('Helvetica', 18))
+switch_spot.grid(row=0, column=0, sticky='we', pady=(20,0))
 
 #space:
 advice = Label(mainframe, text='Advice from The Sage:', font=('Helvetica', 18))
@@ -139,13 +153,13 @@ image = image.resize((200,200))
 image = ImageTk.PhotoImage(image)
 
 #button with loaded image:
-uploadImage = Button(mainframe, image=image, command=upload_file, font=('Helvetica', 18), cursor='hand2')
-uploadImage.grid(row=1, column=0, ipadx=.1*root.winfo_screenwidth(), ipady=.18*root.winfo_screenheight(), padx=30, pady=(8,0))
+upload_image = Button(mainframe, image=image, command=upload_file, font=('Helvetica', 18), cursor='hand2')
+upload_image.grid(row=1, column=0, ipadx=.1*root.winfo_screenwidth(), ipady=.18*root.winfo_screenheight(), padx=30, pady=(8,0))
 
 #Text for ai feedback:
-feedbackFont = Font(family='Helvetica',size=24)
+feedback_font = Font(family='Helvetica', size=16)
 
-feedback = Text(mainframe, bg='lightgreen', state='disabled', font=feedbackFont, width=int(.4*root.winfo_screenwidth()/24), height = 10, wrap=WORD)
+feedback = Text(mainframe, bg='lightgreen', state='disabled', font=feedback_font, width=int(.4*root.winfo_screenwidth()/24), height = 10, wrap=WORD)
 feedback.grid(row=1, column=1, ipadx=.08*root.winfo_screenwidth(),  ipady=.135*root.winfo_screenheight(), padx=30, pady=(8,0))
 
 #add button to reset image and feedback
@@ -161,8 +175,7 @@ root.after(100, check_queue())
 
 #run GUI
 root.mainloop()
-
-thread.join()
+running = False
 # note for nate, will delete eventually:
 # padx=(left,right)
 # pady=(top,bottom)
