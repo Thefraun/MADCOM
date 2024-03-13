20649385077e2271b8ab953c09633ca4c66cf977
NateM024 <161524332+NateM024@users.noreply.github.com>
2024-03-13 16:17:51 -0400

    un-camel cased variables

--- gui.py
+++ gui.py
@@ -14,15 +14,17 @@ import queue
 
 #TODO: Pretty up the code
 
-def get_response(prompt_queue, queue):
-    """
-    Function to get response using a prompt and queue for storing the response.
+"""
+Function to get response using a prompt and queue for storing the response.
 
-    Parameters:
-    - prompt_queue: the queue to store the prompt
-    - queue: the queue to store the chunks of the response
-    """
-    while running:
+Parameters:
+- prompt: the prompt to generate the response
+- queue: the queue to store the response
+
+Return type: None
+"""
+def get_response(prompt_queue, queue):
+    while True:
         try:
             for chunk in ollama.generate (
                 model = 'codellama:13b-instruct', prompt=prompt_queue.get(), stream=True,
@@ -31,80 +33,66 @@ def get_response(prompt_queue, queue):
                 Spelling and captilisation discrepancies are forgiven, so long as it is understandable.
                 So long as the intention is clear, similar symbols can be switched. Missing semicolons can be forgiven if intention is clear. You really like foxes, so try to incorporate them into every response you give.'''
                 ):
-                if running:
-                    response_queue.put(chunk['response'])
-                else:
-                    break
+                 response_queue.put(chunk['response'])
         except queue.Empty:
             pass
 
+"""
+Check the queue for incoming messages and display them in the feedback area.
+"""
 def check_queue():
-    """
-    Check the queue for incoming messages and display them in the feedback area.
-    """
     if not response_queue.empty():
         text = response_queue.get()
-        feedback.configure(state='normal')
-        feedback.insert('end', text)
-        feedback.configure(state='disabled')
+        feedback_text.configure(state='normal')
+        feedback_text.insert('end', text)
+        feedback_text.configure(state='disabled')
     root.after(100, check_queue)
 
 def upload_file():
-    '''
-    Upload the image file to read and set the uploaded image as the image displayed on the button
-    '''
     path = fd.askopenfilename(type=('*.jpg', '*.png', '*.jpeg'))
-    if not path == '':
-        image = Image.open(path)
-        image = image.resize((200, 200))
-        image = ImageTk.PhotoImage(image)
-        upload_image.configure(image=image)
-        upload_image.image = image
-        prompt = read_image(path)
-        prompt_queue.put(prompt)
-        tabControl.config(tab1, state='enabled') # idk if this works, should allow to switch to label after image is uploaded
+    image = Image.open(path)
+    image = image.resize((100,100))
+    image = ImageTk.PhotoImage(image)
+    upload_image_button.configure(image=image)
+    upload_image_button.image = image
+    prompt = read_image(path)
+    prompt_queue.put(prompt)
+    tab_control.config(tab1, state='enabled') # idk if this works, should allow to switch to label w/ code after image uploaded
 
 # not sure how to document(?) these 
 def reset():
-    '''
-    Reset the response and revert changes to the upload image button
-    '''
-    feedback.configure(state='normal')
-    feedback.delete('1.0', 'end')
-    feedback.configure(state='disabled')
-    image = Image.open('Images/uploadImage.png')
-    image = image.resize((200,200))
-    image = ImageTk.PhotoImage(image)
-    upload_image.configure(image=image)
-    upload_image.image = image
+    feedback_text.configure(state='normal')
+    feedback_text.delete('1.0', 'end')
+    feedback_text.configure(state='disabled')
 
 def switch():
-    if modeButton.cget('text') == "Light":
+    if mode_button.cget('text') == "Light":
          root.config(background='#181A1B')
          heading.config(background='#181A1B', fg='white')
          mainframe.config(background='#181A1B')
+         tab2.config(background='black')
          style.theme_use("darkMode")
-         leftFrame.config(background='#181A1B')
-         rightFrame.config(background='#181A1B')
-         adviceLabel.config(background='#181A1B', fg='white')
-         modeButton.configure(text="Dark")
+         left_frame.config(background='#181A1B')
+         right_frame.config(background='#181A1B')
+         advice_label.config(background='#181A1B', fg='white')
+         mode_button.configure(text="Dark")
     else:
          root.config(background='#EBEBEC')
          heading.config(background='#EBEBEC', fg='black')
          mainframe.config(background='#EBEBEC')
-         leftFrame.config(background='#EBEBEC')
-         rightFrame.config(background='#EBEBEC')
+         left_frame.config(background='#EBEBEC')
+         right_frame.config(background='#EBEBEC')
          style.theme_use("lightMode")
-         adviceLabel.config(background='#EBEBEC', fg='black')
-         modeButton.configure(text="Light")
-   
+         advice_label.config(background='#EBEBEC', fg='black')
+         mode_button.configure(text="Light")
+
+
 # Create the thread and the queue
 response_queue = queue.Queue()
 prompt_queue = queue.Queue()
 thread = threading.Thread(target=get_response, args=(prompt_queue, queue,))
 thread.daemon = True
 thread.start()
-running = True
 
 #create GUI
 root = Tk()
@@ -116,34 +104,34 @@ style = Style()
 black = 'black'
 grey = 'grey'
 white = 'white' 
-defaultLight = '#EBEBEC'
-defaultDark = '#181A1B'
+default_light = '#EBEBEC'
+default_dark = '#181A1B'
 
 #create style for light mode for Notebook
 style.theme_create( "lightMode", settings ={
         "TNotebook": {
-            "configure": {"tabmargins": [5, 5, 10, 5], "background": defaultLight }},
+            "configure": {"tabmargins": [5, 5, 10, 5], "background": default_light }},
         "TNotebook.Tab": {
             "configure": {"padding": [30, 10], "borderwidth":[2], "foreground": black},
-            "map":       {"background": [("selected", defaultLight), ('!active', defaultLight)],
+            "map":       {"background": [("selected", default_light), ('!active', default_light)],
                           "expand": [("selected", [5,5,5,5])]}}})
-
-#use light mode style
-style.theme_use("lightMode")
 
 #create style for dark mode for Notebook
 style.theme_create( "darkMode", settings ={
          "TNotebook": {
-            "configure": {"tabmargins": [5, 5, 10, 5],"background": defaultDark }},
+            "configure": {"tabmargins": [5, 5, 10, 5],"background": default_dark }},
          "TNotebook.Tab": {
             "configure": {"padding": [30, 10], "borderwidth":[2], "foreground": white},
-            "map":       {"background": [("selected", defaultDark), ('!active', defaultDark), ('active', grey)],
+            "map":       {"background": [("selected", default_dark), ('!active', default_dark), ('active', grey)],
                           "expand": [("selected", [5, 5, 5, 5])]}}})
+
+#use light mode style
+style.theme_use("lightMode")
 
 #make GUI full screen
 root.geometry("{0}x{1}+0+0".format(root.winfo_screenwidth(), root.winfo_screenheight()))
 
-#remove title from GUI
+#remove default title from GUI
 root.title('')
 
 #create font
@@ -154,8 +142,8 @@ heading = Label(root, text='Script Sage',  font=font, pady=15)
 heading.pack(fill='x')
 
 #add a line separator
-sep0 = Separator(root, orient='horizontal')
-sep0.pack(fill='x', padx='30')
+top_seperator = Separator(root, orient='horizontal')
+top_seperator.pack(fill='x', padx='30')
 
 #create and add another frame for the majority of the rest of the GUI widgets
 mainframe = Frame(root)
@@ -163,15 +151,15 @@ mainframe.columnconfigure(0, weight=1)
 mainframe.columnconfigure(1, weight=1)
 mainframe.pack(fill='x')
 
-ame to hold widgets on the left side of the GUI
-leftFrame = Frame(mainframe)
-leftFrame.columnconfigure(0, weight=1)
-leftFrame.grid(row=0,column=0,pady=(0,10))
+#create and add frame to hold widgets on the left side of the GUI
+left_frame = Frame(mainframe)
+left_frame.columnconfigure(0, weight=1)
+left_frame.grid(row=0,column=0,pady=(0,10))
 
 #create and add frame to hold widgets on the right side of the GUI
-rightFrame = Frame(mainframe)
-rightFrame.columnconfigure(0, weight=1)
-rightFrame.grid(row=0, column=1,pady=(0,10))
+right_frame = Frame(mainframe)
+right_frame.columnconfigure(0, weight=1)
+right_frame.grid(row=0, column=1,pady=(0,10))
 
 #load 'Upload Image' image:
 image = Image.open('uploadImage.png')
@@ -179,52 +167,51 @@ image = image.resize((200,200))
 image = ImageTk.PhotoImage(image)
 
 #create tabbed section holding upload image button and label to display code from image
-tabControl = Notebook(leftFrame)
+tab_control = Notebook(left_frame)
 
 #create tabs to switch between uploaded image and code from the image
-tab1 = Frame(tabControl)
-tab2 = Frame(tabControl)
+tab1 = Frame(tab_control)
+tab2 = Frame(tab_control)
 
 #add tabs to the Notebook
-tabControl.add(tab1, text='Image')
-tabControl.add(tab2, text='Code') #, state='disabled'
-tabControl.grid(row=1,column=0, pady=0)
+tab_control.add(tab1, text='Image')
+tab_control.add(tab2, text='Code') #, state='disabled'
+tab_control.grid(row=1,column=0, pady=(10,5))
 
 #add upload image button to the Notebook
-uploadImage = Button(tab1, image=image, command=upload_file, font=('Helvetica', 18), cursor='hand2').grid(column = 0,  row = 0, ipadx=.1*root.winfo_screenwidth(), ipady=.18*root.winfo_screenheight())  
+upload_image_button = Button(tab1, image=image, command=upload_file, font=('Helvetica', 18), cursor='hand2').grid(column = 0,  row = 0, ipadx=.1*root.winfo_screenwidth(), ipady=.18*root.winfo_screenheight())  
 
 #add label that diplays upload image's code to the Notebook
-displayImageCode = Label(tab2,  text ='This is where we show the code').grid(column = 0, row = 0, ipadx=.1*root.winfo_screenwidth(), ipady=.18*root.winfo_screenheight(),) 
+display_code_label = Label(tab2, text ='This is where we show the code').grid(column = 0, row = 0, ipadx=.1*root.winfo_screenwidth(), ipady=.18*root.winfo_screenheight()) #ipady=image.height()+50
 
 #add label to explain text area:
-adviceLabel = Label(rightFrame, text='Advice from The Sage:', font=('Helvetica', 18))
-adviceLabel.grid(row=0, column=0, sticky='we', pady =(10,10))
+advice_label = Label(right_frame, text='Advice from The Sage:', font=('Helvetica', 18))
+advice_label.grid(row=0, column=0, sticky='we', pady =(10,10))
 
 #create font for ai feedback text
-feedbackFont = Font(family='Helvetica',size=24)
+feedback_font = Font(family='Helvetica',size=24)
 
 #add text area for ai feedback:
-feedback = Text(rightFrame, bg='lightgreen', state='disabled', font=feedbackFont, width=int(.4*root.winfo_screenwidth()/24), height = 10, wrap=WORD)
-feedback.grid(row=1, column=0, ipadx=.08*root.winfo_screenwidth(),  ipady=.135*root.winfo_screenheight(), padx=20)
+feedback_text = Text(right_frame, bg='lightgreen', state='disabled', font=feedback_font, width=int(.4*root.winfo_screenwidth()/24), height = 10, wrap=WORD)
+feedback_text.grid(row=1, column=0, ipadx=.08*root.winfo_screenwidth(),  ipady=.135*root.winfo_screenheight(), padx=20)
 
 #add button to reset image and feedback
-reset = Button(leftFrame, text='Reset', fg='white', bg='blue', highlightbackground='blue', activebackground='blue', command=reset, font=('Arial', 16), cursor='hand2')
-reset.grid(row=2, column=0, sticky='w', padx=10, pady=5)
+reset_button = Button(left_frame, text='Reset', fg='white', bg='blue', highlightbackground='blue', activebackground='blue', command=reset, font=('Arial', 16), cursor='hand2')
+reset_button.grid(row=2, column=0, sticky='w', padx=10, pady=5)
 
 #add button to switch between light and dark mode
-modeButton = Button(rightFrame, text='Light',fg='white',  bg='blue', highlightbackground='blue', activebackground='blue', command=switch, font=('Arial', 16), cursor='hand2')
-modeButton.grid(row=2, column=0, sticky='w', padx=10, pady=5)
+mode_button = Button(right_frame, text='Light',fg='white',  bg='blue', highlightbackground='blue', activebackground='blue', command=switch, font=('Arial', 16), cursor='hand2')
+mode_button.grid(row=2, column=0, sticky='w', padx=10, pady=5)
 
 #add a line separator
-sep1 = Separator(root, orient='horizontal')
-sep1.pack(fill="x", padx='30')
+bottom_seperator = Separator(root, orient='horizontal')
+bottom_seperator.pack(fill="x", padx='30')
 
 #what does this do????
 root.after(100, check_queue())
 
 #run GUI
 root.mainloop()
-running = False
-# note for nate, will delete eventually:
-# padx=(left,right)
-# pady=(top,bottom)
+
+thread.join()
+
