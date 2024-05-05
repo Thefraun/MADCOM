90e299090e74d7e33384e413c0a6b6cca78d55c5
Thefraun <logan@ratpackstudios.com>
2024-05-05 19:58:46 -0400

    Prepares this branch for merge and picture taking

--- gui.py
+++ gui.py
@@ -8,7 +8,6 @@ from tkinter import filedialog as fd
 from PIL import Image, ImageTk
 from cloud_services import read_image, text_to_speech
 from ai import AI
-import time
 import threading
 
 #TODO: Make the thing work, especially code label
@@ -78,8 +77,8 @@ class GUI:
         self.upload_image_button.image = image
         
         # Changes the display code label back to its original text
-        self.display_code_text.configure(state='normal')
-        self.display_code_text.delete('1.0', 'end')
+        self.code_text.configure(state='normal')
+        self.code_text.delete('1.0', 'end')
         
         # Makes the read_aloud_button invisible
         self.display_hidden_buttons(False)
@@ -99,7 +98,7 @@ class GUI:
                 self.reset()
             
             # Begins a thread of the function send_prompt to read the prompt and send it to the AI
-            prompt_thread = threading.Thread(target=self.send_prompt, args=(path,), name='prompt_thread')
+            prompt_thread = threading.Thread(target=self.send_uploaded_prompt, args=(path,), name='prompt_thread')
             prompt_thread.daemon = True
             prompt_thread.start()
             
@@ -120,8 +119,14 @@ class GUI:
         """
         Sends the prompt to vision.py for processing and displays the read prompt in the GUI
         """
-        self.display_code_text.configure(state='disabled')
-        prompt = self.display_code_text.get('1.0', 'end')
+        if not self.feedback_text.get('1.0', 'end').__len__() == 0:
+            self.feedback_text.configure(state='normal')
+            self.feedback_text.delete('1.0', 'end')
+            self.feedback_text.configure(state='disabled')
+            self.display_hidden_buttons(False)
+            
+        self.code_text.configure(state='disabled')
+        prompt = self.code_text.get('1.0', 'end')
         ai.set_prompt(prompt)
         
     def send_uploaded_prompt(self, path):
@@ -130,8 +135,8 @@ class GUI:
         """
         prompt = read_image(path)
         ai.set_prompt(prompt)
-        self.display_code_text.insert('start', prompt)
-        self.display_code_text.configure(state='disabled')
+        self.code_text.insert('1.0', prompt)
+        self.code_text.configure(state='disabled')
         
     def display_hidden_buttons(self, make_visible):
         """
@@ -139,7 +144,7 @@ class GUI:
         """
         if make_visible:
             self.read_aloud_button.grid(row=0, column=0, sticky='e', pady=(0,5))
-            self.details_button.grid(row=2, column=0, sticky='w', pady=5)
+            self.details_button.grid(row=2, column=0, sticky='w', pady=(0,5))
         else:
             self.read_aloud_button.grid_forget()
             self.details_button.grid_forget()
@@ -157,10 +162,10 @@ class GUI:
                 self.feedback_text.configure(state='normal')
                 self.feedback_text.insert('end', chunk)
                 self.feedback_text.see('end')
-                self.feedback_text.configure(state='disabled')
                 read_aloud_visible = False
             elif ai.get_has_completed() and not read_aloud_visible:
                 self.display_hidden_buttons(True)
+                self.code_text.configure(state='normal')
                 read_aloud_visible = True
                 
     def follow_up(self):
@@ -194,7 +199,7 @@ class GUI:
                 'TNotebook': {
                     'configure': {'tabmargins': [5, 5, 10, 5], 'background': self.default_light }},
                 'TNotebook.Tab': {
-                    'configure': {'padding': [30, 10], 'borderwidth':[2], 'foreground': black, 'font': tab_font},
+                    'configure': {'padding': [25, 10], 'borderwidth':[2], 'foreground': black, 'font': tab_font},
                     'map':       {'background': [('selected', self.default_light), ('!active', self.default_light)],
                                 'expand': [('selected', [5, 5, 5, 5])]}}})
 
@@ -204,7 +209,7 @@ class GUI:
                 'TNotebook': {
                     'configure': {'tabmargins': [5, 5, 10, 5],'background': self.default_dark }},
                 'TNotebook.Tab': {
-                    'configure': {'padding': [30, 10], 'borderwidth':[2], 'foreground': white, 'font': tab_font},
+                    'configure': {'padding': [25, 10], 'borderwidth':[2], 'foreground': white, 'font': tab_font},
                     'map':       {'background': [('selected', self.default_dark), ('!active', self.default_dark), ('active', grey)],
                                 'expand': [('selected', [5, 5, 5, 5])]}}})
 
@@ -226,7 +231,7 @@ class GUI:
 
         #create and add a space to help keep the top widgets spaced out
         self.space_label = Label(self.top_frame, text='Light Mode', font=('Arial', 16), fg=self.default_light)
-        self.space_label.grid(row=0, column=0, padx=(10,0))
+        self.space_label.grid(row=0, column=0, padx=(138,0))
 
         # Create font for the heading
         heading_font = Font(family='Helvetica', size=36, weight='bold')
@@ -239,7 +244,7 @@ class GUI:
 
         # Create and add a button to switch between light and dark mode
         self.mode_button = Button(self.top_frame, text='Dark Mode',fg='white', bg='#056939', highlightbackground='#056939', activebackground='#056939', command=self.mode_toggle, font=('Arial', 16), cursor='hand2')
-        self.mode_button.grid(row=0, column=2, sticky='e', padx=(0,105), pady=(90,0))
+        self.mode_button.grid(row=0, column=2, sticky='e', padx=(0,138))
 
         # Create and add a line separator
         self.top_seperator = Separator(self.root, orient='horizontal')
@@ -266,6 +271,11 @@ class GUI:
         upload_image = upload_image.resize((550, 600))
         upload_image = ImageTk.PhotoImage(upload_image)
         
+        # Create/load 'Take Picture' image:
+        take_picture_image = Image.open('Images/takePicture.png')
+        take_picture_image = take_picture_image.resize((550, 600))
+        take_picture_image = ImageTk.PhotoImage(take_picture_image)
+        
         # Create and add tabbed section holding upload image button and label to display code from image
         self.tab_control = Notebook(self.left_frame)
 
@@ -277,20 +287,22 @@ class GUI:
         # Add tabs to the Notebook
         self.tab_control.add(self.picture_tab, text='Take a Picture')
         self.tab_control.add(self.upload_tab, text='Upload Image')
-        self.tab_control.add(self.code_tab, text='Code')
+        self.tab_control.add(self.code_tab, text='Write Code')
         self.tab_control.grid(row=1,column=0, pady=(10,5))
+        
+        # Create and add the take picture button to the Notebook
+        self.picture_tab_button = Button(self.picture_tab, image=take_picture_image, command=self.upload_taken_picture, cursor='hand2')
+        self.picture_tab_button.grid(column=0, row=0)
 
         # Create and add the upload image button to the Notebook
         self.upload_image_button = Button(self.upload_tab, image=upload_image, command=self.upload_file, cursor='hand2')
         self.upload_image_button.grid(column=0, row=0)  
 
-        # Create and add a label that diplays upload image's code to the Notebook
-        self.display_code_text = Text(self.code_tab, wrap=WORD, font=('Futura', 16), width=46, height=25)
-        self.edit_code_button = Button(self.code_tab, text='Edit Code', cursor='hand2', width=77, height=3, bg="#056939", fg=white, command=self.send_written_prompt)
+        # Create and add a text field and button to submit written code to the Notebook
+        self.code_text = Text(self.code_tab, wrap=WORD, font=('Futura', 16), width=46, height=25)
+        self.edit_code_button = Button(self.code_tab, text='Submit Code', cursor='hand2', width=77, height=3, bg="#056939", fg=white, command=self.send_written_prompt)
         self.edit_code_button.grid(column=0, row=0, sticky='s')
-        self.display_code_text.grid(column=0, row=0)
-        
-        # Create and add a label that displays the image taken from the camera
+        self.code_text.grid(column=0, row=0)
 
         # Create and add a label to explain text area
         self.advice_label = Label(self.right_frame, text='Advice from The Sage:', font=('Futura', 20))
@@ -308,14 +320,14 @@ class GUI:
 
         # Create and add a text area for ai feedback:
         self.feedback_text = Text(self.right_frame, bg='lightgray', state='disabled', font=self.feedback_font, width=46, height=22, wrap=WORD)
-        self.feedback_text.grid(row=1, column=0)
+        self.feedback_text.grid(row=1, column=0, pady=(0,15), sticky='ns')
         
         # Create follow up button
         self.details_button = Button(self.right_frame, text='Follow Up', fg='white', bg='#056939', highlightbackground='#056939', activebackground='#056939', command=self.follow_up, font=('Arial', 16), cursor='hand2')
 
         # Create and add a button to reset image and feedback
         self.reset_button = Button(self.left_frame, text='Reset', fg='white', bg='#056939', highlightbackground='#056939', activebackground='#056939', command=self.reset, font=('Arial', 16), cursor='hand2')
-        self.reset_button.grid(row=2, column=0, sticky='w', pady=5)
+        self.reset_button.grid(row=2, column=0, sticky='w', pady=(10,5))
 
         # Create and add a line separator
         self.bottom_seperator = Separator(self.root, orient='horizontal')
